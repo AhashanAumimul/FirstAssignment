0a986a3879cbd6650381945a2eca12a847726f15
Aumimul Ahoshan <ahoshanaumimul@gmail.com>
2025-12-26 20:46:22 +0600

    Fix print statements for Set elements in main

--- dart.dart
+++ dart.dart
@@ -1,12 +1,13 @@
 void main(){
   Set<int> A={1,2,3,4,5};
   Set<int> B={4,5,6,7,8};
-  print("The element of Set A is:");
+  print("The elements in Set A is:");
   for(int item in  A){
     print(item);
   }
-  print("The element in Set B is:");
+  print("The elements in Set B is:");
   for(int item in B){
     print(item);
   }
+
 }
